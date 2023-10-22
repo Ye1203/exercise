5a57567def5b6706b8bdff69b174cf936ce63346
Ye1203 <biangtian@icloud.com>
2023-10-22 12:43:08 -0400

    commit1

--- exercise.R
+++ exercise.R
@@ -1,23 +1,3 @@
-library(dplyr)
-strawberry=read.csv("strawberry_oct4.csv",header=T)
-strwb_census= strawberry|>
-  filter(Program=="CENSUS")|>
-  select(Value!="(D)")
-
-strwb_survey= strawberry|>
-  filter(Program=="SURVEY")
-
-vals=strwb_census$Value
-vals=ifelse(vals=="(D)",NA,vals)
-vals=sub(",","",vals)
-vals=sub('""',"",vals)
-vals=as.numeric(vals)
-vals
-strwb_census["Value"]=vals
-
-vals=strwb_survey$Value
-vals=ifelse(vals=="(D)",NA,vals)
-vals=sub('""',"",vals)
-vals=as.numeric(vals)
-vals
-strwb_survey["Value"]=vals
+library(ggplot2)
+library(esquisse)
+esquisser(year_survey)
